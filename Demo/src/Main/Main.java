5207969b224dca1aef9b50c770fafae4928e4112
Kamil <kamil_grinholc@o2.pl>
2015-06-23 11:04:56 +0200

    New engine

--- Demo/src/Main/Main.java
+++ Demo/src/Main/Main.java
@@ -1,10 +1,11 @@
 package Main;
-
+import parts.*;
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello!!!");
-
+		Engine x = new Engine(23.0,"GTC");
+		System.out.println("New Engine: " + x.getModel() + "  " + x.getHorsePower());
 	}
 
 }
